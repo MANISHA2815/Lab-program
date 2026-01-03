445284820b93158827cccb0f91bf60018a6a6a64
MANISHA2815 <24bcs027@kgcas.com>
2026-01-03 10:49:15 +0530

    Todolist add&delete

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,74 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tasks, setTasks] = useState([]);
+  const [text , setText] = useState(" ");
+
+  const addTask = (e) => {
+    e.preventDefault();
+    const trimmed = text.trim();
+    if(!trimmed)return;
+
+    const newTask={
+       id:Date.now(),
+       text: trimmed,
+       completed:false
+    };
+
+    setTasks([...tasks, newTask]);
+    setText(" ");
+  };
+
+         const deleteTask = (id)=>{
+          setTasks(tasks.filter((t)=>t.id !== id));
+         };
+
+  const toggleTask = (id) => {
+    setTasks( 
+      tasks.map((t)=>
+        t.id===id?{...t, completed:!t.completed}:t
+    )
+    );
+  };
 
   return (
-    <>
+    <div className='app'>
+      <h1>To-Do-List</h1>
+      <form onSubmit={addTask} className='task-form'>
+        <input 
+        value={text}
+        onChange={(e)=>setText(e.target.value)}
+        placeholder='Enter new task'
+        aria-label='task'
+        />
+        <button type='submit'>Add</button>
+      </form>
+      <ul className='task-list'>
+        {task.length===0 && <li className='empty'>No tasks yet</li>}
+        {task.map((task)=>(
+          <li key={task.id} className='task-name'>
+            <input 
+            type="checkbox" checked={task.completed} 
+            onChange={()=>toggleTask(task.id)}
+            />
+            <span onClick={()=>toggleTask(task.id)}
+              className={task.completed?"done":""}>
+                {task.text}
+           </span>
+           <button className='delete' onClick={()=>deleteTask(task.id)}></button>
+            Delete
+          </li>
+        ))}
+
+      </ul>
+
+    
+    </div>
+  )
+}
+
+export default App
+/*<>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -27,9 +91,5 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+      </p> 
+      </>*/
