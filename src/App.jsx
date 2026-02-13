386a9ac9fa121e8b8bbc24d4bfb32b8247863f24
MANISHA2815 <24bcs027@kgcas.com>
2026-02-13 09:50:30 +0530

    calculator

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Greeting from './compontents/welcome/Greeting'
+import { Calculator } from './compontents/calc/calculator'
 
 function App(){
   const [count, setCount]=useState()
   return(
-    <Greeting/>
+    <Calculator/>
   )
 }
 
